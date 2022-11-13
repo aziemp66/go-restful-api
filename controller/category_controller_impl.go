e543b3199b73c18f1fe62600a40332c44331f718
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-13 23:27:49 +0700

    Category Controller Implementation

---
+++ controller/category_controller_impl.go
@@ -0,0 +1,132 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/aziemp66/go-restful-api/helper"
+	"github.com/aziemp66/go-restful-api/model/web"
+	"github.com/aziemp66/go-restful-api/service"
+	"github.com/julienschmidt/httprouter"
+)
+
+type CategoryControllerImpl struct {
+	CategoryService service.CategoryService
+}
+
+func (c CategoryControllerImpl) Create(
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+) {
+	decoder := json.NewDecoder(r.Body)
+	categoryCreateRequest := web.CategoryCreateRequest{}
+
+	err := decoder.Decode(&categoryCreateRequest)
+	helper.PanicIfError(err)
+
+	categoryResponse := c.CategoryService.Create(r.Context(), categoryCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Value:  categoryResponse,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+
+func (c CategoryControllerImpl) Update(
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+) {
+	//TODO implement me
+	decoder := json.NewDecoder(r.Body)
+	categoryUpdateRequest := web.CategoryUpdateRequest{}
+
+	err := decoder.Decode(&categoryUpdateRequest)
+	helper.PanicIfError(err)
+
+	categoryId := p.ByName("id")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	categoryUpdateRequest.Id = id
+
+	categoryResponse := c.CategoryService.Update(r.Context(), categoryUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Value:  categoryResponse,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+
+func (c CategoryControllerImpl) Delete(
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+) {
+	categoryId := p.ByName("id")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	c.CategoryService.Delete(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+
+func (c CategoryControllerImpl) FindById(
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+) {
+	categoryId := p.ByName("id")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	categoryResponse := c.CategoryService.FindById(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Value:  categoryResponse,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+
+func (c CategoryControllerImpl) FindAll(
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+) {
+	categoryResponse := c.CategoryService.FindAll(r.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Value:  categoryResponse,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
